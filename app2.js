dd74b5297e626581e333efbb858449016ff1d594
helxsz <helxsz@gmail.com>
2013-12-04 09:07:29 +0000

    update

--- app2.js
+++ app2.js
@@ -1,52 +1,156 @@
-var fs = require('fs'),
-    request = require("request"),
-	mqtt = require('mqtt');
-	
-var olddata;
-setInterval(readFile, 4000);
+/***********
+http://stackoverflow.com/questions/18484775/how-do-you-access-an-amazon-sns-post-body-with-express-node-js
+http://aws.amazon.com/developers/access-keys/
+http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/frames.html#!AWS/SNS.html
+http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/frames.html
+http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html
+http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-examples.html
+************/
+var argv = require('optimist').argv;
+console.log('(%d,%d)', argv.x, argv.y);
 
-var mqttclient = mqtt.createClient(1883, "test.mosquitto.org");
-mqttclient.on('connect', function(){
-    console.log('MQTT Connected'.green, new Date());
+
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+AWS.config.loadFromPath('config.json');
+
+/*
+var s3 = new AWS.S3();
+var bucketName = 'node-sdk-sample-' + uuid.v4();
+var keyName = 'hello_world.txt';
+
+s3.createBucket({Bucket: bucketName}, function() {
+  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+  s3.putObject(params, function(err, data) {
+    if (err)
+      console.log(err)
+    else
+      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+  });
+});
+
+s3.listBuckets(function(error, data) {
+  if (error) {
+    console.log(error); // error is Response.error
+  } else {
+    console.log(data); // data is Response.data
+	for (var index in data.Buckets) {
+        var bucket = data.Buckets[index];
+        console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+    }	
+  }
+});
+
+var params = {'Bucket': 'myBucket', 'Key': 'myKey','Expires': 60}; 
+var url = s3.getSignedUrl('getObject', params);
+console.log("The URL is", url);
+console.log(new Date());
+*/
+
+
+/*  
+var params = {Bucket: 'myBucket', Key: 'myImageFile.jpg'};
+var file = require('fs').createWriteStream('/path/to/file.jpg');
+s3.getObject(params).createReadStream().pipe(file);
+
+s3.getObject(params).
+on('httpData', function(chunk) { file.write(chunk); }).
+on('httpDone', function() { file.end(); }).
+send();
+*/
+
+
+
+/*  SNS 
+AWS Free Tier availability:
+
+1 million Mobile Push Notifications
+100 SMS
+1,000 email/email-JSON
+100,000 HTTP/s
+unlimited deliveries to SQS Queues
+
+
+
+http://blog.matoski.com/articles/snssqs-for-node-js/
+http://dev.classmethod.jp/cloud/amazonsns-http/
+http://aws.amazon.com/documentation/sns/
+http://docs.aws.amazon.com/sns/latest/dg/SNSMobilePush.html
+http://docs.aws.amazon.com/sns/latest/dg/SendMessageToSQS.html
+
+
+var sns = new AWS.SNS();
+
+// subscribe
+sns.Subscribe({topic: "topic", Protocol: "https"}, function (err, data) {
+    if (err) {
+        console.log(err); // an error occurred
+    } else {
+        console.log(data); // successful response - the body should be in the data
+    }
 });
 
 
-function readFile(){
-    fs.readFile('c:\\k.txt', 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log(data);
-		
-		mqttclient.publish('parking/1', data);
-		
-		/*
-		if(olddata !=data){
-		    console.log('send new data');
-			olddata = data;
-		}	
-			var url;
-			
+    // publish example
+sns.Publish({topic: "topic", message: "my message"}, function (err, data) {
+    if (err) {
+        console.log(err); // an error occurred
+    } else {
+        console.log(data); // successful response - the body should be in the data
+    }
+});
+*/
+ 
+ 
 
-            url = 'http://localhost:8080/parking_spots?data='+data;
-			request.get(url, function(error, response, body) {
-			    if (!error && response.statusCode == 200) {
-                      console.log(body) // Print the google web page.
-                }else{
-				     console.log(error);
-				}
-			})
-			
-		    
-            url = 'http://54.212.249.167:80/parking_spots?data='+data;
-			request.get(url, function(error, response, body) {
-			    if (!error && response.statusCode == 200) {
-                      console.log(body) // Print the google web page.
-                }else{
-				     console.log(error);
-				}
-			})
-		*/
-			
-    });
-}
+ 
+var ec2 = new AWS.EC2();
+
+var params = {
+  //ImageId: 'ami-1624987f', // Amazon Linux AMI x86_64 EBS
+  ImageId: "ami-2d4aa444",
+  InstanceType: 't1.micro',
+  MinCount: 1, MaxCount: 1
+};
+
+// Create the instance
+ec2.runInstances(params, function(err, data) {
+  if (err) { console.log("Could not create instance", err); return; }
+
+  var instanceId = data.Instances[0].InstanceId;
+  console.log("Created instance", instanceId);
+
+  // Add tags to the instance
+  params = {Resources: [instanceId], Tags: [
+    {Key: 'Name', Value: instanceName}
+  ]};
+  ec2.createTags(params, function(err) {
+    console.log("Tagging instance", err ? "failure" : "success");
+  });
+});
+
+ /*
+
+
+
+ app.post('/compressit',function(req,res) {
+    try{
+        var jsp = require("uglify-js").parser;
+        var pro = require("uglify-js").uglify;
+ 
+        var orig_code = req.param("js");
+        var ast = jsp.parse(orig_code); // parse code and get the initial AST
+        ast = pro.ast_mangle(ast); // get a new AST with mangled names
+        ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
+        ast = pro.ast_lift_variables(ast);
+        var final_code = pro.gen_code(ast,{inline_script:true});
+        res.send(final_code);
+    }
+    catch(err) {
+        res.send("Error:" +err.message);
+    }
+});
+
+
+Read more: http://jaspreetchahal.org/build-a-javascript-compressor-tool-using-nodejs-expressjs-jade-uglifyjs-tutorial/#ixzz2m4c00BYD
+ */
--- routes/beta_api.js
+++ routes/beta_api.js
@@ -357,48 +357,57 @@ function EventDetailHandler(err,obj){
 
 f99864c3e8bf55b2de28d76fca76d10e
 ***************************************************************************/	
+//subscribe();
+
+function subscribe(){
     // mqtt://1bfc8d081f5b1eed8359a7517fdb054a:@geras.1248.io//enlight/Ballast0000291B/dolFinTemperature
     var mqttclient = mqtt.createClient(1883, "geras.1248.io",{username:'1bfc8d081f5b1eed8359a7517fdb054a' ,password: "" });
   
     mqttclient.on('connect', function(){
         console.log('MQTT Connected'.green, config.key, config.pattern);
-	    mqttclient.subscribe('//enlight/Ballast0000291B/light');
+	    mqttclient.subscribe('/enlight/Ballast0000291B/light');
     });
 
     mqttclient.on('message', function (topic, message) {
         console.log('mqtt message'.green,  message, "    ",new Date());
-		/*
-		 io.sockets.emit('mqtt',
-           {   'topic'  : topic,
-              'payload' : message
-           }
-        );
-		*/
     });
 
+}	
+	
+	
 
 //subscribeToMQTT(armhome);
-//subscribeToMQTT(armmeeting);
-//subscribeToMQTT(enlight);
-function subscribeToMQTT(config){
-
+subscribeToMQTT(armmeeting);
+subscribeToMQTT(enlight,'/Ballast0000291B/light');
+function subscribeToMQTT(config,name){
+    name = name || '/#';
     var mqttclient = mqtt.createClient(1883, "geras.1248.io",{username:config.key ,password: "" });
   
     mqttclient.on('connect', function(){
-        console.log('MQTT Connected'.green, config.key, config.pattern);
-	    mqttclient.subscribe(config.pattern+'/#');
+        console.log('MQTT Connected'.green, '/'+config.pattern+name);
+	    mqttclient.subscribe('/'+config.pattern+name);
     });
 
     mqttclient.on('message', function (topic, message) {
         console.log('mqtt message'.green,  message, "    ",new Date());
-		/*
-		 io.sockets.emit('mqtt',
-           {   'topic'  : topic,
-              'payload' : message
-           }
-        );
-		*/
+		/**/
+		 io.sockets.emit('mqtt',{'payload' : message});
+		
     });
+	
+	mqttclient.on('disconnect', function(packet) {
+        console.log('disconnect!',packet);
+    });
+
+    mqttclient.on('close', function(packet) {
+        console.log('close!',packet);
+	});
+
+    mqttclient.on('error', function(err) {
+        console.log('error!',err);
+    });
+	
+	
 }
 
 
@@ -423,10 +432,20 @@ io.configure(function () {
        ]);	
 });
 io.sockets.on('connection', function (socket) {
+    console.log('socket connected');
+	socket.emit('connect');
+	
+	io.sockets.emit('mqtt',
+           {   'topic'  : 'aaa',
+              'payload' : 'ccc'
+           }
+        );
+	
+    /*
     socket.on('subscribe', function (data) {
         mqttclient.subscribe(data.topic);
     });
-
+    */
 });
 
 
@@ -494,160 +513,3 @@ function publishMsg( channel , mssage){
     redisClient.publish(channel,JSON.stringify({'test':'test'}));
 	redisClient.quit();
 }
-
-
-var data = {
-    e:[
-        {n:"humidity", v:22.5, t:12345},
-        {n:"humidity", v:23.5, t:24680},
-        {n:"lightcc", v:100, t:12345},
-        {n:"lightcc", v:200, t:13692}
-    ]
-};
-//  small chunks
-
-// big chunks
-// http://data.openiot.org/cat
-//  https://dev.1248.io:8002/cats/ARMAlertMe (key = ADMINSECRET)
-//  http://212.49.230.229:8000/cats/ARM1 (no key)
-//  http://geras.1248.io/cat/enlight (key = 1bfc8d081f5b1eed8359a7517fdb054a) 
-/*
-This is being republished by 1248's Geras as the catalogue http://geras.1248.io/cat/enlight (key = 1bfc8d081f5b1eed8359a7517fdb054a)
-The republished catalogue provides SenML resources which support time and rollup queries, allowing apps to fetch just the data they need.
-You can see an example of this by using the browser, http://dev.1248.io:8000/browser.html?url=https://geras.1248.io/cat/enlight&key=1bfc8d081f5b1eed8359a7517fdb054a
-
-The AlertMe and Enlight data can both be accessed directly from Geras using the keys (924a7d4dbfab38c964f5545fd6186559 and 1bfc8d081f5b1eed8359a7517fdb054a respectively).
-The examples of doing this are in the client-side HTML+JS apps I shared last Friday (https://www.dropbox.com/sh/btilo6qsqyjwluv/ilhsMU5MFA)
-Geras allows time and rollup queries as well as subscribing using MQTT/WebSockets.
-To view the full Geras API, signup for an account at http://geras.1248.io, then login and see the API tab.
-
-I would suggest that, given the short timeframe for the 29th, you start by modifying the apps I shared last Friday - which access known data series from Geras.
-For example, motion.html and energy.html should get you most of what you need for the ARM app presentation you shared.
-
-http://212.49.230.229:8000/cats/ARM1       ........To access ARM cat from us.
-http://dev.1248.io:8001/cats/ARM1            .........To access ARM cat through Toby's PathFinder.
-
-"https://dev.1248.io:8002/cats/ARMAlertMe?key=f99864c3e8bf55b2de28d76fca76d10e", //
-    //url:"http://212.49.230.229:8000/cats/ARM1",
-	//url:"http://geras.1248.io/cat/enlight?key=1bfc8d081f5b1eed8359a7517fdb054a",
-
-*/
-
-/*
-request.get({
-    url: 'http://geras.1248.io/cat',
-    headers: {
-        'Authorization': 'Basic ' + new Buffer('f99864c3e8bf55b2de28d76fca76d10e:').toString('base64')
-    }
-}, function(error, response, body) {
-	if(error)
-	console.log("error  ".red, error);
-	else
-	console.log("HyperCat discovery".green,body);
-});
-
-
-request.post({
-    url: "http://geras.1248.io/series/abc",
-	body: JSON.stringify(data),
-    headers: {
-        'content-type':'application/json',
-        'Authorization': 'Basic ' + new Buffer('f99864c3e8bf55b2de28d76fca76d10e:').toString('base64')
-    }
-}, function(error, response, body) {
-	if(error)
-	console.log("error  ".red, error);
-	else
-	console.log("post series ".green,body);
-});
-
-//https://geras.1248.io/public/armhome_16_SkyDisplay_00-0D-6F-00-00-F2-62-A5_signal
-//https://geras.1248.io/public/armhome_1_MeterReader_00-0D-6F-00-00-F2-6E-23_energy?key=ADMINSECRET
-
-request.get({
-    url: 'http://geras.1248.io/serieslist',
-    headers: {
-        'Authorization': 'Basic ' + new Buffer('f99864c3e8bf55b2de28d76fca76d10e:').toString('base64')
-    }
-}, function(error, response, body) {
-	if(error)
-	console.log("error  ".red, error);
-	else
-	console.log("response get serieslist".green,body);
-});
-
-
-request.get({
-    url: 'http://geras.1248.io/serieslist?pattern='+encodeURIComponent('/abc/+'),
-    headers: {
-        'Authorization': 'Basic ' + new Buffer('f99864c3e8bf55b2de28d76fca76d10e:').toString('base64')
-    }
-}, function(error, response, body) {
-	if(error)
-	console.log("error  ".red, error);
-	else
-	console.log("List series matching MQTT pattern".green,body);
-});
-
-
-
-function getHistoryDataOnDevice(url,key){
-    request.get({
-        url: 'http://geras.1248.io/now/abc/lightcc',
-        headers: {
-            'Authorization': 'Basic ' + new Buffer('f99864c3e8bf55b2de28d76fca76d10e:').toString('base64')
-        }
-    }, function(error, response, body) {
-	    if(error)
-	    console.log("error  ".red, error);
-	    else
-	    console.log("Read most recent value".green,body);
-    });
-}
-
-function getRealTimeData(url,key){
-    request.get({
-        url: 'http://geras.1248.io/series/foo/temperature?start=1234&end=2468',
-        headers: {
-            'Authorization': 'Basic ' + new Buffer('f99864c3e8bf55b2de28d76fca76d10e:').toString('base64')
-        }
-    }, function(error, response, body) {
-	    if(error)
-	    console.log("error  ".red, error);
-	    else
-	    console.log("Time windowing".green,body);
-    });
-}
-
-
-
-
-request.get({
-    url: 'http://geras.1248.io/series/abc/lightcc',
-    headers: {
-        'Authorization': 'Basic ' + new Buffer('f99864c3e8bf55b2de28d76fca76d10e:').toString('base64')
-    }
-}, function(error, response, body) {
-	if(error)
-	console.log("error  ".red, error);
-	else
-	console.log("Read single series data".green,body);
-});
-
-
-request.get({
-    url: 'http://geras.1248.io/series/abc?recursive',
-    headers: {
-        'Authorization': 'Basic ' + new Buffer('f99864c3e8bf55b2de28d76fca76d10e:').toString('base64')
-    }
-}, function(error, response, body) {
-	if(error)
-	console.log("error  ".red, error);
-	else
-	console.log("Read single recursive data".green,body);
-});
-
-
-
-
-*/
