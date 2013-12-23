8f58e9bf35cdd6a6138da68e7def47545de05456
Steven Hassall <steven@redninja.co.uk>
2013-12-23 11:57:06 +0000

    Index, CSS, JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
+//magnificPopup
+$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
 
 // Emulate hover on tablets and smartphones
 $( ".ch-item" ).click(function(e) {
@@ -8,3 +13,4 @@ $( ".ch-item" ).click(function(e) {
 $('.noSpin').click(function(event){
     event.stopImmediatePropagation();
 });
+
