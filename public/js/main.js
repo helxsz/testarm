b5a953997cfbf076383847ad60b4aca9aff67886
Steven Hassall <steven@redninja.co.uk>
2014-01-07 16:15:27 +0000

    css, js, html - scrollbar fix

--- public/js/main.js
+++ public/js/main.js
@@ -19,3 +19,16 @@ $('.noSpin').click(function(event){
     event.stopImmediatePropagation();
 });
 
+// fix height bug on .container as there was an annoying gap below
+
+$(function(){
+    var bodyHeight = $('body').height();
+    var containerHeight = $('#container').height();
+    
+    if (bodyHeight > containerHeight){
+        $('#container').css('height', bodyHeight );
+    }
+    else{
+        $('body').css('height', containerHeight);
+    }
+});
