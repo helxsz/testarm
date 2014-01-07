621215bca43fe6744494ec45b1d78c68e20fb323
Steven Hassall <steven@redninja.co.uk>
2014-01-07 15:04:01 +0000

    css

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
 //magnificPopup
 $('.open-popup-link').magnificPopup({
   type:'inline',
-  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  // Delay in milliseconds before popup is removed
+  removalDelay: 300,
+  // Class that is added to popup wrapper and background
+  // make it unique to apply your CSS animations just to this exact popup
+  mainClass: 'mfp-fade'
 });
 
 // Emulate hover on tablets and smartphones
