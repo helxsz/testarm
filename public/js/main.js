e989af64e13b209c7d79ed519ac218c825ab013b
Steven Hassall <steven@redninja.co.uk>
2014-01-22 17:46:59 +0000

    main.js

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,41 @@ $(function(){
         $('body').css('height', containerHeight);
     }
 });
+
+// Emulate hover on tablets and smartphones
+$( ".ch-item" ).click(function(e) {
+  $(this).toggleClass( "flipped" );
+  e.preventDefault();
+});
+
+$('.noSpin').click(function(event){
+    event.stopImmediatePropagation();
+});
+
+
+// touch device overflow fix
+function isTouchDevice(){
+    try{
+        document.createEvent("TouchEvent");
+        return true;
+    }catch(e){
+        return false;
+    }
+}
+
+function touchScroll(id){
+    if(isTouchDevice()){ //if touch events exist...
+        var el=document.getElementById(id);
+        var scrollStartPos=0;
+
+        document.getElementById(id).addEventListener("touchstart", function(event) {
+            scrollStartPos=this.scrollTop+event.touches[0].pageY;
+            event.preventDefault();
+        },false);
+
+        document.getElementById(id).addEventListener("touchmove", function(event) {
+            this.scrollTop=scrollStartPos-event.touches[0].pageY;
+            event.preventDefault();
+        },false);
+    }
+}
