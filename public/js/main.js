ce3ded5a728a9cb55a6e8228b65e54d9d512302b
Steven Hassall <steven@redninja.co.uk>
2014-01-23 11:50:01 +0000

    main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,21 @@
+//console.log('START main.js');
 
+//Dynamically assign height
+function sizeContent() {
 
-// fix height bug on .container as there was an annoying gap below
-$(function(){
+    // fix height bug on .container as there was an annoying gap below
     var bodyHeight = $('body').height();
-    var containerHeight = $('#container').height();
+    var containerHeight = $("#container").height();
     
-    if (bodyHeight > containerHeight){
-        $('#container').css('height', bodyHeight );
+    if (bodyHeight > containerHeight) {
+      $('#container').css('height', bodyHeight );
     }
-    else{
-        $('body').css('height', containerHeight);
+    else {
+      $('body').css('height', containerHeight);
     }
-});
+    //console.log ('sizeContent working');
+
+}
 
 // Emulate hover on tablets and smartphones
 $( ".ch-item" ).click(function(e) {
@@ -24,41 +28,13 @@ $('.noSpin').click(function(event){
 });
 
 
-// touch device overflow fix
-function isTouchDevice(){
-    try{
-        document.createEvent("TouchEvent");
-        return true;
-    }catch(e){
-        return false;
-    }
-}
-
-function touchScroll('#container'){
-    if(isTouchDevice()){ //if touch events exist...
-        var el=document.getElementById(id);
-        var scrollStartPos=0;
-
-        document.getElementById(id).addEventListener("touchstart", function(event) {
-            scrollStartPos=this.scrollTop+event.touches[0].pageY;
-            event.preventDefault();
-        },false);
-
-        document.getElementById(id).addEventListener("touchmove", function(event) {
-            this.scrollTop=scrollStartPos-event.touches[0].pageY;
-            event.preventDefault();
-        },false);
-    }
-}
-
 //Initial load of page
 $(document).ready(sizeContent);
 
 //Every resize of window
 $(window).resize(sizeContent);
 
-//Dynamically assign height
-function sizeContent() {
-    var newHeight = $("html").height() + "px";
-    $("#container").css("height", newHeight);
-}
+// touch device overflow fix
+
+
+//console.log ('END main.js');
