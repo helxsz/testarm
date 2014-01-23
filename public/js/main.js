8e2fe7d0ae5cb880fd56631faa871212e6ddbaf8
Steven Hassall <steven@redninja.co.uk>
2014-01-23 11:58:51 +0000

    main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
-//console.log('START main.js');
+console.log('START main.js');
 
+
+function init(){
+
+     touchScroll("container");
+     sizeContent();
+}
 //Dynamically assign height
 function sizeContent() {
 
@@ -13,7 +19,7 @@ function sizeContent() {
     else {
       $('body').css('height', containerHeight);
     }
-    //console.log ('sizeContent working');
+    console.log ('sizeContent working');
 
 }
 
@@ -29,12 +35,36 @@ $('.noSpin').click(function(event){
 
 
 //Initial load of page
-$(document).ready(sizeContent);
+$(document).ready(init);
 
 //Every resize of window
-$(window).resize(sizeContent);
+$(window).resize(init);
 
 // touch device overflow fix
+function isTouchDevice() {
+    try{
+        document.createEvent("TouchEvent");
+        return true;
+    }catch(e){
+        return false;
+    }
+}
 
+function touchScroll(id) {
+    if(isTouchDevice()){ //if touch events exist...
+        var el=document.getElementById(id);
+        var scrollStartPos=0;
 
-//console.log ('END main.js');
+        document.getElementById(id).addEventListener("touchstart", function(event) {
+            scrollStartPos=this.scrollTop+event.touches[0].pageY;
+            event.preventDefault();
+        },false);
+
+        document.getElementById(id).addEventListener("touchmove", function(event) {
+            this.scrollTop=scrollStartPos-event.touches[0].pageY;
+            event.preventDefault();
+        },false);
+    }
+}
+
+console.log ('END main.js');
